17e7f16d94c2be32b93ef6fa2521268a470cf565
PeterJBloch <peterjbloch@gmail.com>
2021-03-09 15:42:24 -0800

    Implemented test_fourhundreds and fixed code to work with the test in LeapYear.py

--- LeapYear.py
+++ LeapYear.py
@@ -1,6 +1,8 @@
 def LeapYear(number):
     if number%4 ==0:
         if number%100 ==0:
+            if number%400==0:
+                return True
             return False
         return True
     return False

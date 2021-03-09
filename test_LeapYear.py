2b199faf2dd5e4eb733f6a5c2c68276dd87dd795
PeterJBloch <peterjbloch@gmail.com>
2021-03-09 15:30:22 -0800

    Wrote second test for LeapYear, test_hundreds

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -7,6 +7,11 @@ class Test_FizzBuzz(unittest.TestCase):
         self.assertEqual(LeapYear.LeapYear(8),True)
         self.assertEqual(LeapYear.LeapYear(3),False)
 
+    def test_hundreds(self):
+        self.assertEqual(LeapYear.LeapYear(200),False)
+        self.assertEqual(LeapYear.LeapYear(300),False)
+    
+
 
 if __name__ == "__main__":
     unittest.main()
